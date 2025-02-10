857a27dc3b95bd5570102d3a8ae2a6298ac49659
hum39k <2162923@student.uma.pt>
2025-02-10 11:47:43 +0000

    aula3

--- Ficha1/server.js
+++ Ficha1/server.js
@@ -16,15 +16,16 @@
 
 }
 
-calculategrade(12,10,5)
-calculategrade(2,15,15)
-calculategrade(12,15,16)
-calculategrade(20,19,19)
+// calculategrade(12,10,5)
+// calculategrade(2,15,15)
+// calculategrade(12,15,16)
+// calculategrade(20,19,19)
 
 
 /*asd
-asdas
-asda*/
+comentario 
+asd*/
+
 
 function data(num){
 
@@ -57,3 +58,89 @@ function data(num){
 }
 
 // data(1)
+
+
+
+function operacao(n1,n2,op){
+
+    if (op == "+")
+        return n1 + n2
+    else if (op == "-")
+        return n1 - n2  
+    else if (op == "*")
+        return n1 * n2
+    else if (op == "/")
+        return n1 / n2         
+}
+
+
+// console.log(operacao(7,2,"*"))
+
+function multiple(num,max){
+    
+    for (var i =1; i < max; i++){
+        
+        if (i % num == 0)
+            console.log(i) 
+    }
+}
+
+// multiple(5,20)
+
+
+function soma(n){
+    var res = 1;
+
+    for(var i =2; i <= n; i++){
+        res+=i;
+    }
+
+    return res;
+}
+
+// console.log(soma(5));
+
+function fatorial(n){
+    var res = 1;
+
+    for(var i =n; i >=1; i--){
+        res*=i;
+    }
+
+    return res;
+}
+
+// console.log(fatorial(5))
+
+
+function max(list){
+    var nmax = list[0];
+    for(let i = 1; i < list.length; i++){
+        if (list[i] > nmax)
+            nmax = list[i]
+    }
+    return nmax
+}
+
+function min(list){
+    var nmin = list[0];
+    for(let i = 1; i<list.length; i++){
+        if (list[i] < nmin)
+            nmin = list[i]
+    }
+    return nmin
+}
+
+function media(list){
+    var res = 0;
+   
+    for(let i=0; i < list.length; i++){
+        res+= list[i];
+    }
+    med = res / list.length;
+    return med;
+}
+
+console.log(min([15,3,75,2,11]));
+
+
